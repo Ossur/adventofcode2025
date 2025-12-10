a920d3bfbb929a13f04073357c92164be3f1ba11
Össur Jónsson <ossurj@pearlgroup.is>
2025-12-10 10:22:25 +0000

    day1 part 2

--- Day1/SecretEntrance.kts
+++ Day1/SecretEntrance.kts
@@ -27,3 +27,45 @@ fun readInputAndCount(inputname: String): Int {
 
 println(readInputAndCount("./test_input.txt"))
 println(readInputAndCount("./input.txt"))
+
+
+
+fun readInputAndCountZeroHits(inputname: String): Int {
+	var clicks = 50
+	var onZero = 0
+	File(inputname).forEachLine { line ->
+		val num = line.substring(1).toInt()
+		val wasZero = clicks == 0
+		clicks = when(line[0]) {
+			'L' -> clicks - num
+			'R' -> clicks + num
+			else -> clicks
+		}
+		clicks = when {
+			clicks < 0 -> {
+				if (!wasZero) {
+					onZero++
+				}
+				onZero += (clicks.div(-100))
+				eucledian_modulus(clicks, 100)
+			}
+			clicks >= 100 -> {
+				onZero += (clicks.div(100))
+				eucledian_modulus(clicks, 100)
+			}
+			clicks == 0 -> {
+				onZero++
+				clicks
+			}
+			else -> {
+				clicks
+			}
+		}
+
+	}
+	return onZero
+}
+
+
+println(readInputAndCountZeroHits("./test_input.txt"))
+println(readInputAndCountZeroHits("./input.txt"))
